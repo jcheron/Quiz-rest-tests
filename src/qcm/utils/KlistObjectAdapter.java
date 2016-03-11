c7dd39f041681ec18f37ccf9d53999dd961e1061
jcheron <myaddressmail@gmail.com>
2016-03-11 02:08:37 +0100

    klistobject gson deserializer

--- src/qcm/utils/KlistObjectAdapter.java
+++ src/qcm/utils/KlistObjectAdapter.java
@@ -1,20 +1,39 @@
 package qcm.utils;
 
 import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
 
 import net.ko.kobject.KListObject;
 import net.ko.kobject.KObject;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+public class KlistObjectAdapter
+		implements JsonSerializer<KListObject<? extends KObject>>,
+		JsonDeserializer<KListObject<? extends KObject>> {
 
-public class KlistObjectAdapter implements JsonSerializer<KListObject<? extends KObject>> {
+	@Override
+	public JsonElement serialize(KListObject<? extends KObject> list, Type arg1, JsonSerializationContext context) {
+		JsonElement elm = context.serialize(list.asAL());
+		return elm;
+	}
 
-@Override
-public JsonElement serialize(KListObject<? extends KObject> list, Type arg1, JsonSerializationContext context) {
-	JsonElement elm = context.serialize(list.asAL());
-	return elm;
-}
+	@Override
+	public KListObject<? extends KObject> deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+		Type listType = new TypeToken<List<? extends KObject>>() {
+		}.getType();
+		List<? extends KObject> l = context.deserialize(json.getAsJsonArray(), listType);
+		KListObject<? extends KObject> kl = new KListObject<>(KObject.class);
+		for (KObject o : l) {
+			kl.add(o, true);
+		}
+		return kl;
+	}
 
 }
--- src/quiz/rest/tests/UserTest.java
+++ src/quiz/rest/tests/UserTest.java
@@ -1,47 +1,48 @@
 package quiz.rest.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
-import net.ko.framework.Ko;
-import net.ko.framework.KoHttp;
-import net.ko.framework.KoSession;
-import net.ko.kobject.KListObject;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import qcm.models.KUtilisateur;
-import qcm.utils.HttpUtils;
-import qcm.utils.MyGsonBuilder;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import net.ko.framework.Ko;
+import net.ko.framework.KoSession;
+import net.ko.kobject.KListObject;
+import qcm.models.KUtilisateur;
+import qcm.utils.HttpUtils;
+import qcm.utils.MyGsonBuilder;
+
 public class UserTest {
 	private static Gson gson;
 	private static String baseUrl;
+
 	@BeforeClass
-	public static void setUp(){
-		gson=MyGsonBuilder.create();
-		baseUrl="http://127.0.0.1:8080/Quiz-Rest/rest/";
+	public static void setUp() {
+		gson = MyGsonBuilder.create();
+		baseUrl = "http://127.0.0.1:8080/Quiz-Rest/rest/";
 		Ko.kstart();
 	}
-	
+
 	@Test
 	public void testGetAll() {
 		try {
-			KListObject<KUtilisateur> usersFromDb=KoSession.kloadMany(KUtilisateur.class);
-			String jsonUsers=HttpUtils.getHTML(baseUrl+"user/all");
-			
-			Type listType = new TypeToken<List<KUtilisateur>>()
-	                {
-	                }.getType();
-			List<KUtilisateur> users=gson.fromJson(jsonUsers, listType);
+			KListObject<KUtilisateur> usersFromDb = KoSession.kloadMany(KUtilisateur.class);
+			String jsonUsers = HttpUtils.getHTML(baseUrl + "user/all");
+			Type listType = new TypeToken<List<KUtilisateur>>() {
+			}.getType();
+			List<KUtilisateur> users = gson.fromJson(jsonUsers, listType);
 			assertEquals(users.size(), usersFromDb.count());
+			for (int i = 0; i < users.size(); i++) {
+				assertEquals(users.get(i).getLogin(), usersFromDb.get(i).getLogin());
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
